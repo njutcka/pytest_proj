c174614132e020e0249ba94c527016f7380aded3
AnnaK <njutcka@yandex.ru>
2023-08-06 13:02:56 +0700

    задание 1.5 добиться чтобы тесты работали

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,10 +1,13 @@
 from utils import arrs
-
+import pytest
 
 def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 3
-    assert arrs.get([], 0, "test") == "test"
+    assert arrs.get([1, 2, 3], 1, 2) == 2
 
+
+def test_get_index_error():
+    with pytest.raises(IndexError):
+        arrs.get([], 0)
 
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
